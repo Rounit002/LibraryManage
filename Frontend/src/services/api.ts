c69ffdb2684d2f3bebcfd1e700788530c580294f
Rounit <rounitraj747@gmail.com>
2025-04-15 18:25:19 +0530

    api changed

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://librarymanage-sm1b.onrender.com/api';
 
 // Create an Axios instance with credentials support
 const apiClient = axios.create({
